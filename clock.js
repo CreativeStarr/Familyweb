5e9d4c3aeddb56446d72b4fbe49ffa1156948dfa
Donald Starr <starrallendonald@gmail.com>
2022-07-31 20:45:17 -0400

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,8 @@
+
+
+setInterval(myTimer, 1000);
+
+function myTimer() {
+  const d = new Date();
+  document.getElementById("time").innerHTML = d.toLocaleTimeString();
+}
---
+++ site.php
@@ -0,0 +1,112 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>Home</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta name="Arthor" content="Donald Starr" />
+    <meta name="description" content="A website about my family and also have some song quotes, Music videos, and funny stuff" />
+		<link rel="stylesheet" href="css/style01.css" />
+	</head>
+	<body  class="pgbkgnd1">
+		<header>
+						<h1 class="hdbrd2" id="mytop" align="center"> Creative Starr WebSite </h1>
+		</header>
+		<hr/>
+
+      <nav>
+        <div class="nv2" >
+          <div class="brd3">
+            <span id="time"></span>
+          </div>
+          <h2 class="txt3" align="center">CREATIVE HOME </h2><br/>
+          <span class="txt1"><a href="CreaStarrHome.html" > Creative Home</a></span>
+          <span class="txt1"><a href="Create Account Page.html"> Create Account </a></span>
+          <span class="txt1"><a href="Login page.html"> Login </a></span>
+          <span class="txt1"><a href="YTlnkPage.html" target="_blank">YouTube Video link Page</a></span>
+          <span class="txt1"><a href="Nsite.html" > Nsite </a></span>
+          <span class="txt1"><a href="Mpage1.html">Song Quotes</a></span>
+					<span class="txt1"><a href="Mpage2.html">Song Quotes 2</a></span>
+					<span class="txt1"><a href="site.php">Test Site</a></span>
+          <span class="txt1"><a href="mailto:starrdonald@zoominternet.net" > Email </a></span>
+          <span class="txt1"><a href="https://github.com/CreativeStarr/Familyweb.git"> github </a></span>
+        </div>
+      </nav>
+			<br/>
+			<br/>
+			<br/>
+			<?php
+				echo"<h3>Hello Allen, What do you think about php?</h3>";
+
+			?>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/><br/>
+			<div class="brd2">
+					 <p><a href="#mytop" class="txt1">Back to Top</a></p><br/>
+			</div>
+			<br/><br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<footer>
+				<div class="ft1">
+					<p class="txt5">Thanks for visiting</p>
+					<p class="txt6">I learning HTML and CSS from watching videos by <a Style="color:gold" href= "https://www.mikedane.com" target= "_blank">The Mike's website</a></P>
+					<p class="txt6">Referance and resources was used from  <a Style="color:gold" href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
+					<p  class="txt6">Some elements of HTML & CSS was used from template webpages from <a Style="color:gold" href="https://templated.co"target="_blank">TEMPLATED</a></P>
+
+				</div>
+			</footer>
+		<!-- Scripts -->
+
+			<script src="js/Lightsw.js"></script>
+			<script src="js/contentbx.js"></script>
+			<script src="js/clock.js"></script>
+			<script src="js/slipbtn.js"></script>
+	</body>
+</html>
